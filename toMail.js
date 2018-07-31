1e9e657b2bf7b809b17fd4e72af4dc8d77dad5e7
Abdelrahmanm mustafa saleh <am2136@fayoum.edu.eg>
2018-07-31 13:10:51 +0200

    add ability to send reviews to defined mails

---
+++ toMail.js
@@ -0,0 +1,46 @@
+const nodemailer = require('nodemailer');
+const schedule = require('node-schedule');
+const listOfMails = require('./readMails').call();
+
+
+
+
+var smtpTransport = nodemailer.createTransport({
+    host: "smtp.gmail.com", // hostname
+    secureConnection: true, // use SSL
+    port: 465, // port for secure SMTP
+    auth: {
+        user: "abdelrahman.mos.saleh@gmail.com",
+        pass: "0846341563",
+    }
+})
+
+const  sendMail = function  (reviews){
+    return  schedule.scheduleJob('* * 8-18 * * 0-4', function(){
+      
+        for (i = reviews.length - 1; i >= 0; --i) {
+         
+            var mailOptions = {
+                from: "<abdelrahman.mos.saleh@gmail.com>", // sender address
+                to: listOfMails.toString(), // list of receivers
+                subject: "Hello ✔", // Subject line
+                text: reviews[i], // plaintext body
+            }
+    
+            smtpTransport.sendMail(mailOptions, function(error, response){
+                if(error){
+                    console.log(error);
+                }else{
+                    console.log("Message sent: " + response.message);
+                }
+    
+            });
+            
+            reviews.splice(i, 1); // Remove even numbers
+            
+          }
+    });
+    
+}
+
+module.exports = sendMail;
